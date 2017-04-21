9b5f2684bf89010bd2534e461a0806ac93388849
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-20 20:17:46 -0700

    Change seed to spawn on trees

--- bin/glowstone.sh
+++ bin/glowstone.sh
@@ -6,7 +6,9 @@ echo "server:" > /app/config/glowstone.yml
 echo "    port: 25566" >> /app/config/glowstone.yml
 echo "    online-mode: false" >> /app/config/glowstone.yml
 echo "world:" >> /app/config/glowstone.yml
-echo "    seed: '1'" >> /app/config/glowstone.yml
+echo "    seed: '5'" >> /app/config/glowstone.yml
+echo "    allow-nether: false" >> /app/config/glowstone.yml
+echo "    allow-end: false" >> /app/config/glowstone.yml
 
 # Install plugins
 mkdir -p /app/plugins
@@ -18,9 +20,9 @@ mkdir -p /app/plugins/WebSandboxMC
 echo "http:" > /app/plugins/WebSandboxMC/config.yml
 echo "  port: $GS_HEROKU_SERVER_PORT" >> /app/plugins/WebSandboxMC/config.yml
 echo "mc:" >> /app/plugins/WebSandboxMC/config.yml
-echo "  x_center: -451" >> /app/plugins/WebSandboxMC/config.yml
-echo "  y_center: 80" >> /app/plugins/WebSandboxMC/config.yml
-echo "  z_center: -325" >> /app/plugins/WebSandboxMC/config.yml
+echo "  x_center: -72" >> /app/plugins/WebSandboxMC/config.yml
+echo "  y_center: 88" >> /app/plugins/WebSandboxMC/config.yml
+echo "  z_center: -27" >> /app/plugins/WebSandboxMC/config.yml
 
 
 # Sync initial files
