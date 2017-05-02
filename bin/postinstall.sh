253886edeba5c6729166bb6f4847dc8b65374eca
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-05-01 22:18:05 -0700

    Update to Glowstone Light 5

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -2,7 +2,7 @@
 
 # Download Glowstone
 #curl -o vendor/glowstone.jar "https://bamboo.gserv.me/artifact/GSPP-SRV/shared/build-latestSuccessful/Version-Independent-Server-JAR/glowstone.jar"
-curl -Lo vendor/glowstone.jar 'https://github.com/satoshinm/Glowstone/releases/download/vlight.4/glowstone-light.jar'
+curl -Lo vendor/glowstone.jar 'https://github.com/satoshinm/Glowstone/releases/download/vlight.5/glowstone-light.jar'
 
 # Download plugins
 mkdir -p vendor/plugins
